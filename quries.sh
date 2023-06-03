00c4d172e2fbcfe0cf1c9cfe58c2c44ed1b4f042
Varun Chindage <98298554+Va-un@users.noreply.github.com>
2023-06-03 21:42:13 +0530

    Add files via upload

---
+++ quries.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+
+echo "$($PSQL "
+drop table games;
+drop table teams;
+CREATE TABLE teams (
+    team_id SERIAL NOT NULL,
+    name VARCHAR(20) UNIQUE NOT NULL
+);
+
+CREATE TABLE games (
+    game_id SERIAL NOT NULL,
+    year INTEGER NOT NULL,
+    round VARCHAR(20) NOT NULL,
+    winner_id      INTEGER NOT NULL,
+    opponent_id    INTEGER NOT NULL,
+    winner_goals   INTEGER NOT NULL,
+    opponent_goals INTEGER NOT NULL
+);
+
+ALTER TABLE teams ADD PRIMARY KEY (team_id);
+ALTER TABLE games ADD PRIMARY KEY (game_id);
+ALTER TABLE games ADD FOREIGN KEY (winner_id)   REFERENCES teams (team_id);
+ALTER TABLE games ADD FOREIGN KEY (opponent_id) REFERENCES teams (team_id);
+")"
+
+
